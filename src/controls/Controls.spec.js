6f417ae5b7078a46e319750b70f4482a029a3c63
jennl97 <jennhott97@gmail.com>
2019-10-02 22:07:56 -0500

    First commit of web testing 2

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,49 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+//snapshot tests
+test('Controls renders correctly', () => {
+    const locked = jest.fn();
+    const closed = jest.fn();
+
+    render(<Controls 
+        locked={locked}
+        closed={closed}
+        // toggleLocked={this.toggleLocked}
+        // toggleClosed={this.toggleClosed}
+    />);
+});
+
+test('should match snapshot', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
+});
+
+//mocking test
+test('locked functions are called on button clicks', () => {
+    const locked = jest.fn();
+    const closed = jest.fn();
+
+    const { getByText } = render(
+        <Controls 
+            locked={locked}
+            closed={closed}
+            // toggleLocked={this.toggleLocked}
+            // toggleClosed={this.toggleClosed}
+    />
+    );
+
+    getByText(/locked gate/i);
+    getByText(/unlocked gate/i);
+
+    const lockedGateButton = getByText(/locked gate/i);
+    const unlockedGateButton = getByText(/unlocked gate/i);
+
+    fireEvent.click(unlockedGateButton);
+    fireEvent.click(lockedGateButton);
+
+    expect(locked).toHaveBeenCalled();
+    expect(unlocked).toHaveBeenCalled();
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+//snapshot tests
+test('Dashboard renders correctly', () => {
+    render(<Dashboard />);
+});
+
+test('should match snapshot', () =>{
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+//mocking tests
